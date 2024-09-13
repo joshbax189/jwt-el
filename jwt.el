9128a8e90361ffe9d4ea2d40f59d1aedc570851c
Josh Bax <joshbax189@gmail.com>
2024-09-13 12:04:00 -0700

    Display eldoc for defined claims when viewing decoded JWT

--- jwt.el
+++ jwt.el
@@ -30,8 +30,6 @@
 
 ;;; Code:
 
-;; TODO can we display eldoc for defined claims?
-
 (require 'json)
 (require 'cl-lib)
 (require 'hmac-def)
@@ -372,8 +370,10 @@ Specifically it should have
                       (jwt-token-json-header token-json)
                       (jwt-token-json-payload token-json)
                       (jwt-token-json-signature token-json)))
-      (jsonc-mode) ;; TODO perhaps not included?
+      ;; (jsonc-mode) ;; not included -- is it worth including?
+      (js-json-mode)
       (json-pretty-print-buffer)
+      (jwt-minor-mode)
       (pop-to-buffer (current-buffer)))))
 
 (defun jwt-decode-at-point ()
@@ -390,6 +390,40 @@ Specifically it should have
     (unless maybe-token
       (message "No token selected"))
     (jwt-decode maybe-token)))
+
+(defvar jwt--defined-claims-alist
+  '(;; registered claim names
+    (iss "Issuer" "Identifies principal that issued the JWT.")
+    (sub "Subject" "Identifies the subject of the JWT.")
+    (aud "Audience" "Identifies the recipients that the JWT is intended for. Each principal intended to process the JWT must identify itself with a value in the audience claim. If the principal processing the claim does not identify itself with a value in the aud claim when this claim is present, then the JWT must be rejected.")
+    (exp "Expiration Time" "Identifies the expiration time on and after which the JWT must not be accepted for processing. The value must be a NumericDate: either an integer or decimal, representing seconds past 1970-01-01 00:00:00Z.")
+    (nbf "Not Before" "Identifies the time on which the JWT will start to be accepted for processing. The value must be a NumericDate.")
+    (iat "Issued at" "Identifies the time at which the JWT was issued. The value must be a NumericDate.")
+    (jti "JWT ID" "Case-sensitive unique identifier of the token even among different issuers.")
+    ;; header names
+    (typ "Token type"	"If present, it must be set to a registered IANA Media Type.")
+    (cty "Content type" "If nested signing or encryption is employed, it is recommended to set this to JWT; otherwise, omit this field.")
+    (alg "Message authentication code algorithm" "The issuer can freely set an algorithm to verify the signature on the token.")
+    (kid "Key ID" "A hint indicating which key the client used to generate the token signature. The server will match this value to a key on file in order to verify that the signature is valid and the token is authentic.")
+    (x5c "x.509 Certificate Chain" "A certificate chain in RFC4945 format corresponding to the private key used to generate the token signature. The server will use this information to verify that the signature is valid and the token is authentic.")
+    (x5u "x.509 Certificate Chain URL" "A URL where the server can retrieve a certificate chain corresponding to the private key used to generate the token signature. The server will retrieve and use this information to verify that the signature is authentic.")
+    (crit "Critical" "A list of headers that must be understood by the server in order to accept the token as valid"))
+  "Documentation strings for ElDoc.")
+
+(defun jwt--eldoc (callback &rest _ignored)
+  "Document defined claims with ElDoc CALLBACK."
+  (when-let* ((maybe-claim (sexp-at-point))
+              (maybe-doc (cdr (assoc maybe-claim jwt--defined-claims-alist)))
+              (full-name (car maybe-doc))
+              (doco (cadr maybe-doc)))
+    (funcall callback doco :thing full-name)))
+
+(define-minor-mode jwt-minor-mode
+  "Used to display decoded contents of JWTs."
+  :interactive nil
+  :lighter " JWT-decoded"
+  (add-hook 'eldoc-documentation-functions #'jwt--eldoc nil t)
+  (eldoc-mode))
 
 (provide 'jwt)
 
